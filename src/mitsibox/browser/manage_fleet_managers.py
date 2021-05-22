d136f9fe52d284f92aed8e5e18e3f0478a39d6b9
ameurant <alain.meurant@netvaast.be>
2021-05-22 15:08:48 +0200

    Modifications diverses
    - corriger les commentaires, enlever chaufferu et remplacer par fleet manager
    - ajouter fleet manager dans le portal membership
    -

--- src/mitsibox/browser/manage_fleet_managers.py
+++ src/mitsibox/browser/manage_fleet_managers.py
@@ -12,7 +12,7 @@ from connexion_db import ConnexionDb
 
 class ManageFleetManagers(ConnexionDb):
     """
-    gestion des manager des flottes qui doivent 
+    gestion des manager des flottes qui doivent
     recevoir une notification SMS lors du premier dépot
     d'un prélèvement après qu'une boite soit vide
     """
@@ -31,7 +31,7 @@ class ManageFleetManagers(ConnexionDb):
 
     def getFleetManagerById(self, fleetManagerId):
         """
-        Récupères les infos d'un chauffeur selon son ID
+        Récupères les infos d'un fleet-manager selon son ID
         """
         tableFleetManagers = self.getLabDbAccessCollection('mitsibox_fleet_managers')
         recs = tableFleetManagers.find("_id=='%s'" % (fleetManagerId,)).execute()
@@ -41,7 +41,7 @@ class ManageFleetManagers(ConnexionDb):
 
     def getFleetManagerIdByLoginPlone(self, loginPlone):
         """
-        Récupères les infos d'un chauffeur selon son ID dans Plone
+        Récupères les infos d'un fleet-manager selon son ID dans Plone
         """
         if (loginPlone == 'admin'):
             tableFleetManagers = self.getLabDbAccessCollection('mitsibox_fleet_managers')
@@ -51,7 +51,7 @@ class ManageFleetManagers(ConnexionDb):
             myFleetManager = loginPlone
         return myFleetManager
 
-    def createFleetManager(self, fleetManagerId, fleetManagerEmail, fleetManagerFullname, passDriver):
+    def createFleetManager(self, fleetManagerlastName, fleetManagerfirstName, fleetManagerEmail, fleetManagerIdPlone, fleetManagerPassPlone):
         """
         créer un fleetManager dans le plone portal_membership
         """
@@ -59,19 +59,21 @@ class ManageFleetManagers(ConnexionDb):
         membership = getToolByName(self.context, 'portal_membership')
         acl_users = getToolByName(self.context, 'acl_users')
 
+        fleetManagerFullname = fleetManagerfirstName + ' ' + fleetManagerlastName
+
         # password = ''.join(random.choice(chars) for char in range(8))
         properties = {
-            'username': fleetManagerId,
+            'username': fleetManagerIdPlone,
             'email': fleetManagerEmail,
             'fullname': fleetManagerFullname.encode('utf-8')
         }
-        registration.addMember(fleetManagerId, passDriver, properties=properties)
-        acl_users.updateLoginName(fleetManagerId, fleetManagerEmail)
-        return membership.getMemberById(fleetManagerId)
+        registration.addMember(fleetManagerIdPlone, fleetManagerPassPlone, properties=properties)
+        acl_users.updateLoginName(fleetManagerIdPlone, fleetManagerEmail)
+        return membership.getMemberById(fleetManagerIdPlone)
 
     def insertFleetManager(self):
         """
-        insertion d'une nouvelle fleetManagers
+        insertion d'un nouveau fleet-manager
         """
         tableFleetManagers = self.getLabDbAccessCollection('mitsibox_fleet_managers')
 
@@ -81,7 +83,6 @@ class ManageFleetManagers(ConnexionDb):
         fleetManagerfirstName = fields.get('fleetManagerFirstName', None).decode('utf-8')
         fleetManagerIdPlone = fields.get('fleetManagerIdPlone', None)
         fleetManagerPassPlone = fields.get('fleetManagerPassPlone', None)
-        fleetManagerFullName = fleetManagerfirstName + u" " + fleetManagerlastName
         fleetManagerSmsNotification = fields.get('fleetManagerSmsNotification', None)
         fleetManagerGsm = fields.get('fleetManagerGsm', None)
         fleetManagerEmail = fields.get('fleetManagerEmail', None)
@@ -97,11 +98,12 @@ class ManageFleetManagers(ConnexionDb):
 
         tableFleetManagers.add(newFleetManager).execute()
 
-        #self.createFleetManager(fleetManagerId, fleetManagerEmail, fleetManagerFullName, fleetManagerPass)
+        # ajouter le fleet-manager dans control-panle de plone
+        self.createFleetManager(fleetManagerlastName, fleetManagerfirstName, fleetManagerEmail, fleetManagerIdPlone, fleetManagerPassPlone)
 
         portalUrl = getToolByName(self.context, 'portal_url')()
         ploneUtils = getToolByName(self.context, 'plone_utils')
-        message = u"Le fleet manager est enregistrée."
+        message = u"Le fleet manager est enregistré."
         ploneUtils.addPortalMessage(message, 'info')
         url = "%s/administration-de-la-db/gestion-des-gestionnaires-de-flotte/listing-des-gestionnaires-de-flotte" % (portalUrl,)
         self.request.response.redirect(url)
@@ -115,7 +117,7 @@ class ManageFleetManagers(ConnexionDb):
 
         fields = self.request.form
         fleetManagerId = fields.get('fleetManagerId', None)
-        
+
         myFleetManager = {}
         myFleetManager['fleetManagerLastName'] = fields.get('fleetManagerLastName', None).decode('utf-8')
         myFleetManager['fleetManagerFirstName'] = fields.get('fleetManagerFirstName', None).decode('utf-8')
@@ -126,11 +128,10 @@ class ManageFleetManagers(ConnexionDb):
         myFleetManager['fleetManagerPassPlone'] = fields.get('fleetManagerPassPlone', None)
 
         tableFleetManagers.modify("_id='%s'" % fleetManagerId).patch(myFleetManager).execute()
-        #tableDrivers.modify("_id='%s'" % idDriver).patch(myDriver).execute()
 
         portalUrl = getToolByName(self.context, 'portal_url')()
         ploneUtils = getToolByName(self.context, 'plone_utils')
-        message = u"les données du gestionnaire de flotte ont été modifiées."
+        message = u"les données du fleet manager ont été modifiées."
         ploneUtils.addPortalMessage(message, 'info')
         url = "%s/administration-de-la-db/gestion-des-gestionnaires-de-flotte/listing-des-gestionnaires-de-flotte" % (portalUrl,)
         self.request.response.redirect(url)
